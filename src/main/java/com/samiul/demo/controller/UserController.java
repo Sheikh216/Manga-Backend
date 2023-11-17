1f9ab8245e9f0e6f2de85cefa97a17d05df25f01
HumayeraShayera <humayera321@gmail.com>
2023-11-17 23:00:33 +0600

    feature: implemented edit and delete feature of an user

--- src/main/java/com/samiul/demo/controller/UserController.java
+++ src/main/java/com/samiul/demo/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,57 +20,24 @@ public class UserController {
     private UserRepository userRepository;
 
 
-
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
+    User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     } //newuser = {username: samir,username, email, password:123}
 
     @GetMapping("/users")
-    List<User> getAllUsers(){
+    List<User> getAllUsers() {
         return userRepository.findAll();
 
     }
 
 
-//    @GetMapping("/user/{id}")
-//    public User getUserById(@PathVariable Long id) {
-//        return userRepository.findById(id)
-//                .orElseThrow(() -> new UserNotFoundException(id));
-//    }
-
-
-
-
-
-
-
-//    @PostMapping("/login")
-//    public ResponseEntity<String> loginUser(@RequestBody User loginUser) {
-//        Optional<User> userOptional = userRepository.findByUsernameAndPassword(loginUser.getUsername(), loginUser.getPassword());
-//
-//        if (userOptional.isPresent()) {
-//            // User is authenticated, you can generate a JWT token or set a session here
-//            return ResponseEntity.ok("Login successful!");
-//
-//        } else {
-//            // Invalid credentials
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-//
-//        }
-//    }
-// loginuser-->username,name,password
-
     @PostMapping("/login")
     public ResponseEntity<User> loginUser(@RequestBody User loginUser) {
         Optional<User> userOptional = userRepository.findByUsernameAndPassword(loginUser.getUsername(), loginUser.getPassword());
-// user--> username,password
         if (userOptional.isPresent()) {
             // User is authenticated, you can generate a JWT token or set a session here
             User authenticatedUser = userOptional.get();
-
-
-
             authenticatedUser.setLogin(true); // Set login property to true
             userRepository.save(authenticatedUser); // Save the updated user object back to the database
             return ResponseEntity.ok(authenticatedUser); // Return the user object
@@ -80,10 +46,6 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
-
-
-
-
 
 
 //    @PostMapping("/login")
@@ -132,36 +94,26 @@ public class UserController {
         return userOptional.orElse(null); // Returns null if user not found
     }
 
-
-//    @PutMapping("/users/{id}")
-//    public User updateUser(@RequestBody User newUser, @PathVariable Long id) {
-//        return userRepository.findById(id)
-//                .map(user -> {
-//                    user.setUsername(newUser.getUsername());
-//                    user.setName(newUser.getName());
-//                    user.setEmail(newUser.getEmail());
-//                    // If you have other fields to update, add them here
-//                    return userRepository.save(user);
-//                })
-//                .orElseGet(() -> {
-//                    // Handle the case where the user with the given ID is not found
-//                    newUser.setId(id);
-//                    return userRepository.save(newUser);
-//                });
-//    }
-
-
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
 
 
     @DeleteMapping("/user/{id}")
-    public ResponseEntity<String> deleteUserById(@PathVariable Long id) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id);
-            return ResponseEntity.ok("User deleted successfully");
-        } else {
-            // User not found, return a 404 Not Found status code
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+    String deleteuser(@PathVariable Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
         }
+        userRepository.deleteById(id);
+        return "User with " + id + " has been deleted successfully.";
+
     }
 
 }
